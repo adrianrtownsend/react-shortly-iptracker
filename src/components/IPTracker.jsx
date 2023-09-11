475175e84d6e4c8a73a6a96e290e2bcbb6664483
Adrian Townsend <greenlight8004@gmail.com>
2023-09-11 13:38:59 -0400

    v2 changes

--- src/components/IPTracker.jsx
+++ src/components/IPTracker.jsx
@@ -1,39 +1,39 @@
 import { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
-interface IIpData {
-	ip: string;
-	location: {
-		country: string;
-		region: string;
-		city: string;
-		lat: number;
-		lng: number;
-		postalCode: string;
-		timezone: string;
-		geonameId: number;
-	};
-	as: {
-		asn: number;
-		name: string;
-		route: string;
-		domain: string;
-		type: string;
-	};
-	isp: string;
-	proxy: {
-		proxy: boolean;
-		vpn: boolean;
-		tor: boolean;
-	};
-}
+// interface IIpData {
+// 	ip: string;
+// 	location: {
+// 		country: string;
+// 		region: string;
+// 		city: string;
+// 		lat: number;
+// 		lng: number;
+// 		postalCode: string;
+// 		timezone: string;
+// 		geonameId: number;
+// 	};
+// 	as: {
+// 		asn: number;
+// 		name: string;
+// 		route: string;
+// 		domain: string;
+// 		type: string;
+// 	};
+// 	isp: string;
+// 	proxy: {
+// 		proxy: boolean;
+// 		vpn: boolean;
+// 		tor: boolean;
+// 	};
+// }
 
 const IPTracker = () => {
-	const [ipAddress, setIpAddress] = useState<string>('');
-	const [ipData, setIpData] = useState<IIpData | undefined>();
+	const [ipAddress, setIpAddress] = useState('');
+	const [ipData, setIpData] = useState();
 	const mapCoor = { lat: 37.38605, lng: -122.08385 };
 
-	const getLocation = (ipAddress?: string) => {
+	const getLocation = (ipAddress) => {
 		fetch(
 			`https://geo.ipify.org/api/v1?apiKey=${
 				process.env.REACT_APP_LEAFLET_API_KEY
@@ -89,41 +89,43 @@ const IPTracker = () => {
 					</div>
 				</div>
 			</div>
-			<div className='iptracker__map'>
-				<div className='iptracker__stats col-12 px-4'>
-					<div className='iptracker__stats__container col-md-10 mx-auto row'>
-						<div className='col-md-3 iptracker__field'>
-							<p>IP ADDRESS</p>
-							<h3>{ipData ? ipData.ip : '--'}</h3>
-						</div>
-						<div className='col-md-3 iptracker__field'>
-							<p>LOCATION</p>
-							<h3>
-								{ipData
-									? `${ipData.location.city}, ${ipData.location.region} ${ipData.location.postalCode}`
-									: '--'}
-							</h3>
-						</div>
-						<div className='col-md-3 iptracker__field'>
-							<p>TIMEZONE</p>
-							<h3>{ipData ? ipData.location.timezone : '--'}</h3>
-						</div>
-						<div className='col-md-3 iptracker__field'>
-							<p>ISP</p>
-							<h3>{ipData ? ipData.isp : '--'}</h3>
-						</div>
-					</div>
+			<div className='iptracker__stats__container col-md-12 mx-auto row'>
+				<div className='col-md-3 iptracker__field'>
+					<p>IP ADDRESS</p>
+					<h3>{ipData ? ipData.ip : '--'}</h3>
 				</div>
-				{/*<MapContainer
+				<div className='col-md-3 iptracker__field'>
+					<p>LOCATION</p>
+					<h3>
+						{ipData
+							? `${ipData.location.city}, ${ipData.location.region} ${ipData.location.postalCode}`
+							: '--'}
+					</h3>
+				</div>
+				<div className='col-md-3 iptracker__field'>
+					<p>TIMEZONE</p>
+					<h3>{ipData ? ipData.location.timezone : '--'}</h3>
+				</div>
+				<div className='col-md-3 iptracker__field'>
+					<p>ISP</p>
+					<h3>{ipData ? ipData.isp : '--'}</h3>
+				</div>
+			</div>
+			{/* <div className='iptracker__map'>
+				<div className='iptracker__stats col-12 px-4'></div>
+				<MapContainer
 					className='w-100 h-100 iptracker__map'
-					center={[mapCoor.lat, mapCoor.lng]}
+					center={[51.505, -0.09]}
 					zoom={13}
 					scrollWheelZoom={false}
 				>
-					<TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-					<Marker position={[mapCoor.lat, mapCoor.lng]}></Marker>
-        </MapContainer>*/}
-			</div>
+					<TileLayer
+						attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+						url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+					/>
+					<Marker position={[51.505, -0.09]} />
+				</MapContainer>
+			</div> */}
 		</section>
 	);
 };
